e3087f02064d4ce2f504b6f613f5b904ad5d85e7
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-06 23:01:44 +0000

    Add functionality to access private repos

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,17 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token 9ab87dcc755344aff88128c86e02647b5a70323a'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
