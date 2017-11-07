e600cd51a941028d07317a2f1c26aa6a763a47ac
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-07 00:27:19 +0000

    adds a image from a url

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 const request = require('request');
-const token = require('./secrets')
+const token = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -20,10 +21,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
+  //console.log("Errors:", err);
   //console.log("Result:", result);
 
-  result.forEach(function(each) {
-    console.log(each.avatar_url);
-  })
+  // result.forEach(function(each) {
+  //   console.log(each.avatar_url);
+  // })
 });
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+         console.log("Downloading...");
+       })
+       .pipe(fs.createWriteStream(filePath))
+       .on('finish', function () {
+          console.log("Download complete!");
+       });
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
